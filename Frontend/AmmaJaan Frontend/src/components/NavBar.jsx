b46e0cf962fcf370b31ed6db48aad7ef22d0623e
ArshadAman <arshadaman202@gmail.com>
2022-11-21 23:56:36 +0530

    Made the navbar in React.js

---
+++ Frontend/AmmaJaan Frontend/src/components/NavBar.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import logo from "../assets/nav__logo.png";
+import flag from "../assets/indiaflag.png";
+
+function NavBar() {
+  return (
+    <div className="navbar__main bg-[#131921]">
+      <div className="navbar flex px-3 py-2 space-x-10 items-center">
+        <a href="/">
+          {" "}
+          <img
+            src={logo}
+            alt=""
+            className="w-[128.73px] h-[50px] filter invert"
+          />
+        </a>
+        <div className="location text-white cursor-pointer">
+          <p className="pl-4 text-xs text-gray-400">Deliver to Arshad</p>
+          <p>
+            <i className="fas fa-map-marker-alt"></i>{" "}
+            <span className="text-sm font-semibold">Bhubneshwar 7561014</span>
+          </p>
+        </div>
+        <div className="search focus:outline-orange-500 flex items-center">
+          <select
+            name="select__categories"
+            id="select__categories"
+            className="pb-[11px] pt-[11px] px-1 w-16 bg-gray-200 hover:bg-gray-300  focus:outline-orange-500 outline-4 text-[12px] text-gray-500 font-semibold border border-r border-l-0 border-t-0 border-b-0 border-r-black rounded-l"
+          >
+            <option value="all" key="all" className="text-sm text-gray-500">
+              All
+            </option>
+            <option
+              value="amazon_fashion"
+              key="amazon_fashion"
+              className="text-sm text-gray-500"
+            >
+              Amazon Fashion
+            </option>
+            <option
+              value="amazon_fresh"
+              key="amazon_fresh"
+              className="text-sm text-gray-500"
+            >
+              Amazon Fresh
+            </option>
+          </select>
+          <input
+            type="text"
+            className="bg-white py-2 w-[600px] border-0 outline-0 pl-1"
+          />
+          <button type="submit">
+            <i class="fas fa-search px-4 py-[11px] bg-orange-300 text-lg rounded-r"></i>
+          </button>
+        </div>
+        <div className="last flex items-center space-x-10">
+          <p className="language flex items-center text-white cursor-pointer text-sm">
+            <img src={flag} alt="" className="w-[20.6px] h-[20.2px] mr-1" />
+            EN
+          </p>
+          <div className="greet text-white text-xs cursor-pointer">
+            <p className="">Hello Arshad</p>
+            <p>
+              <span className="font-bold text-[13px]">Accounts & Lists</span>
+            </p>
+          </div>
+          <div className="orders text-white text-xs cursor-pointer">
+            <p className="">Returns</p>
+            <p>
+              <span className="font-bold text-[13px]">&Orders</span>
+            </p>
+          </div>
+          <div className="carts text-white flex cursor-pointer">
+            <p className="">
+              <i class="fas fa-shopping-cart text-2xl mr-1"></i>
+            </p>
+            <p>
+              <span className="font-bold text-[13px]">Cart</span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="categories bg-[#232f3e]">
+            <ul className="flex justify-evenly text-white h-[39px] text-sm font-semibold px-10 items-center">
+                <li className="hover:border px-1 py-[0.5px]">All</li>
+                <li className="hover:border px-1 py-[0.5px]">Fresh</li>
+                <li className="hover:border px-1 py-[0.5px]">Amazon Mini Tv</li>
+                <li className="hover:border px-1 py-[0.5px]">Amazon Pay</li>
+                <li className="hover:border px-1 py-[0.5px]">Amazon Gift Cards</li>
+                <li className="hover:border px-1 py-[0.5px]">Buy Again</li>
+                <li className="hover:border px-1 py-[0.5px]">Gift Ideas</li>
+                <li className="hover:border px-1 py-[0.5px]">Health, Household & Personal Care</li>
+                <li className="hover:border px-1 py-[0.5px]">AmazonBasics</li>
+                <li className="hover:border px-1 py-[0.5px]">Subscribe & Save</li>
+                <li className="hover:border px-1 py-[0.5px]">Baby</li>
+                <li className="hover:border px-1 py-[0.5px]">Coupons</li>
+                <li className="hover:border px-1 py-[0.5px]">Toys & Games</li>
+            </ul>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
